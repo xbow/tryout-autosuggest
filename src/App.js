3a62d78c84db4d64b304a4fac3eac110789e1087
Sebastian <sebxian@gmx.de>
2018-11-30 13:18:12 +0100

    Add list of saved suggestions

--- src/App.js
+++ src/App.js
@@ -1,27 +1,129 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Autosuggest from 'react-autosuggest';
+
+/*const testSuggestions = [
+  {
+    title: 'Topics',
+    suggestions: [
+      { name: 'foo' },
+      { name: 'bar' },
+      { name: 'baz' }
+    ]
+  },
+  {
+    title: 'Keywords',
+    suggestions: [
+      { name: 'Fungus' },
+      { name: 'Badabam' },
+      { name: 'Boogeymen' },
+      { name: 'San Francisco' }
+    ]
+  }
+]*/
+
+const testSuggestions = [
+  { name: 'foo' },
+  { name: 'bar' },
+  { name: 'baz' },
+  { name: 'Fungus' },
+  { name: 'Badabam' },
+  { name: 'Boogeymen' },
+  { name: 'San Francisco' }
+]
+
+// Teach Autosuggest how to calculate suggestions for any given input value.
+const getSuggestions = value => {
+  const inputValue = value.trim().toLowerCase()
+  const inputLength = inputValue.length
+
+  return inputLength === 0 ? [] : testSuggestions.filter(tag =>
+    tag.name.toLowerCase().slice(0, inputLength) === inputValue
+  )
+}
+
+// When suggestion is clicked, Autosuggest needs to populate the input
+// based on the clicked suggestion. Teach Autosuggest how to calculate the
+// input value for every given suggestion.
+const getSuggestionValue = suggestion => suggestion.name;
+
+// Use your imagination to render suggestions.
+const renderSuggestion = suggestion => (
+  <div>
+    {suggestion.name}
+  </div>
+)
 
 class App extends Component {
-  render() {
+
+  state = {
+    value: '',
+    savedSuggestions: [],
+    suggestions: []
+  }
+
+  // my own methods
+
+  saveSuggestion = () => {
+    this.setState(
+      {
+        savedSuggestions: [
+          ...this.state.savedSuggestions,
+          this.state.value
+        ]
+      }
+    )
+  }
+
+
+  // methods copied over from
+  // https://www.npmjs.com/package/react-autosuggest
+
+  onChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue
+    })
+  }
+
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: getSuggestions(value)
+    })
+  }
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    })
+  }
+
+  render () {
+
+    const { value, suggestions } = this.state;
+
+    const inputProps = {
+      placeholder: 'Type something starting with f, b or s',
+      value,
+      onChange: this.onChange
+    };
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="saved-suggestions">
+          {this.state.savedSuggestions.map(item => <span className="tag">{item}</span>)}
+        </div>
+        <Autosuggest
+          suggestions={suggestions}
+          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+          onSuggestionSelected={this.saveSuggestion}
+          getSuggestionValue={getSuggestionValue}
+          renderSuggestion={renderSuggestion}
+          inputProps={inputProps}
+        />
       </div>
-    );
+    )
   }
 }
 
